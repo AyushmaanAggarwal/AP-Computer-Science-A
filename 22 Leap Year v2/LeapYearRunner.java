b4b83198c30a0cebeb9b27f952a25fb74406a515
AyushmaanAggarwal <Ayu02468@gmail.com>
2020-12-08 16:08:34 -0500

    22 completed

---
+++ 22 Leap Year v2/LeapYearRunner.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class LeapYearRunner
+{
+	public static void main( String args[] )
+	{		
+		Scanner keyboard = new Scanner(System.in);		
+		System.out.print("Enter a year :: ");
+		int year = keyboard.nextInt();
+		LeapYear ly = new LeapYear();
+		if (ly.isLeapYear(year)) {
+			System.out.println(year+" is a leap year");
+		} else {
+			System.out.println(year+" is NOT a leap year");
+		}
+		//add test cases				
+	}
+}
