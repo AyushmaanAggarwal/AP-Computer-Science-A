96e20a1a720057fc062b76745fb846a43d33efb5
AyushmaanAggarwal <43427654+AyushmaanAggarwal@users.noreply.github.com>
2020-12-08 16:57:47 -0500

    Testing project

--- TestProject/src/Testing/TESTING.java
+++ TestProject/src/Testing/TESTING.java
@@ -1,12 +1,8 @@
-package Testing;
-
 import java.lang.Math;
 
 public class TESTING {
 	public static void main(String[] args) {
 
-		Integer a = 90;
-		Integer c = 90;
-		System.out.println(a==c);
+		System.out.println(true&&false||false);
 	}
 }
