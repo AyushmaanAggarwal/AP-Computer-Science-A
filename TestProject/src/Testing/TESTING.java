7d73a030cea5bc836827ed43ba0b47c055d6226a
AyushmaanAggarwal <43427654+AyushmaanAggarwal@users.noreply.github.com>
2020-12-08 17:28:55 -0500

    Minor Testing Changes

--- TestProject/src/Testing/TESTING.java
+++ TestProject/src/Testing/TESTING.java
@@ -3,6 +3,6 @@ import java.lang.Math;
 public class TESTING {
 	public static void main(String[] args) {
 
-		System.out.println(true&&false||false);
+		System.out.println(true&&false||true);
 	}
 }
