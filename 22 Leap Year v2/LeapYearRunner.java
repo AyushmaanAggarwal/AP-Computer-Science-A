12ab5f7026e8c60e2d2fd42fd0b020c8e2ce8e6e
AyushmaanAggarwal <43427654+AyushmaanAggarwal@users.noreply.github.com>
2020-12-11 15:52:56 -0500

    update

--- 22 Leap Year v2/LeapYearRunner.java
+++ 22 Leap Year v2/LeapYearRunner.java
@@ -3,8 +3,8 @@ import java.util.*;
 public class LeapYearRunner
 {
 	public static void main( String args[] )
-	{		
-		Scanner keyboard = new Scanner(System.in);		
+	{
+		Scanner keyboard = new Scanner(System.in);
 		System.out.print("Enter a year :: ");
 		int year = keyboard.nextInt();
 		LeapYear ly = new LeapYear();
@@ -13,6 +13,15 @@ public class LeapYearRunner
 		} else {
 			System.out.println(year+" is NOT a leap year");
 		}
-		//add test cases				
+		if (ly.isLeapYear(209)) {
+			System.out.println(209+" is a leap year");
+		} else {
+			System.out.println(209+" is NOT a leap year");
+		}
+		if (ly.isLeapYear(2131)) {
+			System.out.println(2131+" is a leap year");
+		} else {
+			System.out.println(2131+" is NOT a leap year");
+		}
 	}
 }
