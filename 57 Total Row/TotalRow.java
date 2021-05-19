da37417470160c9dece2818d87d0dd551ff04e5e
AyushmaanAggarwal <Ayu02468@gmail.com>
2021-05-19 18:05:15 -0400

    Finished 2d Array Total Values

--- 57 Total Row/TotalRow.java
+++ 57 Total Row/TotalRow.java
@@ -4,7 +4,15 @@ public class TotalRow
 {
     public static List<Integer> getRowTotals( int[][] m )
     {
+      ArrayList<Integer> total = new ArrayList<Integer>();
+      for(int[] mRow: m){
+        int tot = 0;
+        for(int mVal: mRow){
+          tot += mVal;
+        }
+        total.add(tot);
+      }
     	//add code here
-		return null;
+		return total;
     }
 }
--- 57 Total Row/TotalRowRunner.java
+++ 57 Total Row/TotalRowRunner.java
@@ -2,7 +2,12 @@ public class TotalRowRunner
 {
 	public static void main( String args[] )
 	{
-		//add code here			
+		int[][] array = {{1,2,3},{5,5,5,5}};
+		int[][] array1 = {{1,2,3},{5},{1},{2,2}};
+		int[][] array2 = {{1,2},{5,5},{5,5},{4,5,6,7},{123124,12312}};
+		System.out.println(TotalRow.getRowTotals(array));
+		System.out.println(TotalRow.getRowTotals(array1));
+		System.out.println(TotalRow.getRowTotals(array2));
 	}
 }
 
