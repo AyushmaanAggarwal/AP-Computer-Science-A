d278994cec59b1cd4c44e65bc3d780eae3bb8162
AyushmaanAggarwal <43427654+AyushmaanAggarwal@users.noreply.github.com>
2021-05-19 17:49:52 -0400

    tr

---
+++ 57 Total Row/TotalRow.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class TotalRow
+{
+    public static List<Integer> getRowTotals( int[][] m )
+    {
+    	//add code here
+		return null;
+    }
+}
---
+++ 57 Total Row/TotalRowRunner.java
@@ -0,0 +1,9 @@
+public class TotalRowRunner
+{
+	public static void main( String args[] )
+	{
+		//add code here			
+	}
+}
+
+
